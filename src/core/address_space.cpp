88f653c866def53c1cc36e81295e3ee52bb25600
okt04175 <okt04175@yahoo.com.hk>
2024-12-13 05:38:03 +0800

    Update address_space.cpp - Fix7

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -108,7 +108,7 @@ struct AddressSpace::Impl {
 
         // Allocate backing file that represents the total physical memory.
         backing_handle =
-            CreateFileMappingW(INVALID_HANDLE_VALUE, FILE_MAP_ALL_ACCESS,
+            CreateFileMappingW(INVALID_HANDLE_VALUE, NULL,
                                PAGE_READWRITE | SEC_COMMIT, BackingSize, BackingSize, nullptr);
         ASSERT_MSG(backing_handle, "{}", Common::GetLastErrorMsg());
         // Allocate a virtual memory for the backing file map as placeholder
