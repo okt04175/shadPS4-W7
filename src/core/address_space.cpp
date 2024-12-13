667b1e15f95623e23a1c040fc467ba051a4d56dc
okt04175 <okt04175@yahoo.com.hk>
2024-12-13 17:24:02 +0800

    Update address_space.cpp - restore backing_base

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -112,13 +112,13 @@ struct AddressSpace::Impl {
                                PAGE_READWRITE | SEC_COMMIT, 0, BackingSize, NULL);
         ASSERT_MSG(backing_handle, "{}", Common::GetLastErrorMsg());
         // Allocate a virtual memory for the backing file map as placeholder
-        //backing_base = static_cast<u8*>(VirtualAllocEx(process, nullptr, BackingSize,
-                                                       //MEM_RESET,
-                                                       //PAGE_NOACCESS));
+        backing_base = static_cast<u8*>(VirtualAllocEx(process, NULL, BackingSize,
+                                                       MEM_RESET,
+                                                       PAGE_NOACCESS));
         // Map backing placeholder. This will commit the pages
         //void* const ret = MapViewOfFileEx(backing_handle, FILE_MAP_ALL_ACCESS, 0, 0,
                                           //BackingSize, backing_base);
-        //ASSERT_MSG(ret == backing_base, "{}", Common::GetLastErrorMsg());
+        ASSERT_MSG(ret == backing_base, "{}", Common::GetLastErrorMsg());
     }
 
     ~Impl() {
