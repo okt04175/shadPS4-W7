92e64147741799c67c9e82ef394937b151a4aed8
okt04175 <okt04175@yahoo.com.hk>
2024-12-15 00:50:19 +0800

    Update address_space.cpp - No Split the placeholder

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -233,11 +233,11 @@ struct AddressSpace::Impl {
         ASSERT(region_size >= minimum_size);
 
         // Split the placeholder.
-        if (!VirtualFreeEx(process, LPVOID(address), 0,
-                           MEM_RELEASE)) {
-            UNREACHABLE_MSG("Region splitting failed: {}", Common::GetLastErrorMsg());
-            return nullptr;
-        }
+        // if (!VirtualFreeEx(process, LPVOID(address), 0,
+                           // MEM_RELEASE)) {
+            // UNREACHABLE_MSG("Region splitting failed: {}", Common::GetLastErrorMsg());
+            // return nullptr;
+        // }
 
         // Do we now have two regions or three regions?
         if (region_size == minimum_size) {
